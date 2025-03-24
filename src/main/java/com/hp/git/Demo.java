29ce1a13acfe2ff039cc6f24c4ee4066c38c2972
Huping Chen <18327184833@163.com>
2025-03-24 17:11:56 +0800

    增加循环破坏标记

--- src/main/java/com/hp/git/Demo.java
+++ src/main/java/com/hp/git/Demo.java
@@ -2,8 +2,15 @@ package com.hp.git;
 
 public class Demo {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+
+        c1:for (int i = 0; i < 10; i++) {
             System.out.println("循环打印");
+            if (i == 4){
+                break c1;
+            }
+            System.out.println("结束");
+
+
         }
     }
 }
