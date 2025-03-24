2dd90401feb346b65c8c05e6e455db00e960589a
hupingChen <18327184833@163.com>
2025-03-24 16:29:23 +0800

    Update Demo.java
    临时修改

--- src/main/java/com/hp/git/Demo.java
+++ src/main/java/com/hp/git/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("本地与远端交互");
         System.out.println("远端交互");
+        System.out.println("远端临时修改");
     }
 }
