6601dc7bc91ffb30f2e31f26be6707d9c6f56494
Huping Chen <18327184833@163.com>
2025-03-24 17:14:28 +0800

    主分支不变更循环

--- src/main/java/com/hp/git/Demo.java
+++ src/main/java/com/hp/git/Demo.java
@@ -2,15 +2,10 @@ package com.hp.git;
 
 public class Demo {
     public static void main(String[] args) {
-
-        c1:for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("循环打印");
-            if (i == 4){
-                break c1;
-            }
-            System.out.println("结束");
-
-
+            System.out.println("主分支没有break");
+            System.out.println("最终合并结果");
         }
     }
 }
