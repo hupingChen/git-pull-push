68d00e1834d8909d74378cf5050968cf40eb91da
Huping Chen <18327184833@163.com>
2025-03-24 17:06:41 +0800

    新分支编写代码功能：循环打印

--- src/main/java/com/hp/git/Demo.java
+++ src/main/java/com/hp/git/Demo.java
@@ -2,20 +2,8 @@ package com.hp.git;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("本地与远端交互");
-        System.out.println("远端交互");
-        System.out.println("远端临时修改");
-        System.out.println("远端分支临时修改");
-        System.out.println("本地测试分支分支临时修改");
-        System.out.println("---------------");
-        System.out.println("testBranch");
-        System.out.println("testBranch分支修改");
-        System.out.println("=========================================");
-        System.out.println("在合并");
-        System.out.println("主分支修改str变量");
-        String env = "字符串变量被修改了";
-        System.out.println("字符串变量被修改了");
-        String str = "字符串变量";
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println("循环打印");
+        }
     }
 }
